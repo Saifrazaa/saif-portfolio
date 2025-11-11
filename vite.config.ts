7aca17a5fe9f266cfc0a10aa95c67d2a75f837fd
saif <saif@saifs-MacBook-Pro.local>
2025-11-11 16:42:00 +0500

    removed homepage url and base url from configurations

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/saif-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
